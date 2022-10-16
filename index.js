556e3c1ef780728c445f26a2d2c60c8cd9cb563b
Dito23 <89190087+anibolokadze@users.noreply.github.com>
2022-10-16 12:26:08 +0400

    mail validation

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+// first form validation
 let emailField = document.getElementById('mail');
 let emailError = document.getElementById('mail-error');
 
+function validateEmail(){
+    if(!emailField.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        emailError.innerHTML = 'Please check your email';
+        emailError.style.color = "red";
+        return false;
+    }
+    emailError.innerHTML = 'This is a valid adress'
+    emailError.style.color = "green";
+    return true;
+}
+
+// second form validation
 let emailField2 = document.getElementById('mail-2');
 let emailError2 = document.getElementById('mail-error-2');
 
-function validateEmail(){
-    if(!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = 'Please check your email';
-        return false;
-    }
-    emailError.innerHTML = '';
-    return true;
-}
 function validateEmail2(){
-    if(!emailField2.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if(!emailField2.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
         emailError2.innerHTML = 'Please check your email';
+        emailError2.style.color = "white";
         return false;
     }
-    emailError2.innerHTML = '';
+    emailError2.innerHTML = 'This is a valid adress';
+    emailError2.style.color = "black";
     return true;
 }
