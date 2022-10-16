4bcb5caa6d8c937c02c90115f3fa5878ae080960
Dito23 <89190087+anibolokadze@users.noreply.github.com>
2022-10-16 12:31:15 +0400

    changes in input color

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let emailError = document.getElementById('mail-error');
 function validateEmail(){
     if(!emailField.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
         emailError.innerHTML = 'Please check your email';
-        emailError.style.color = "red";
+        emailError.style.color = "red"; //error msg color
+        emailField.style.color='black'; //make input text black
         return false;
     }
     emailError.innerHTML = 'This is a valid adress'
@@ -20,7 +21,8 @@ let emailError2 = document.getElementById('mail-error-2');
 function validateEmail2(){
     if(!emailField2.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
         emailError2.innerHTML = 'Please check your email';
-        emailError2.style.color = "white";
+        emailError2.style.color = "white"; //error msg color
+        emailField2.style.color='black'; //make input text black
         return false;
     }
     emailError2.innerHTML = 'This is a valid adress';
